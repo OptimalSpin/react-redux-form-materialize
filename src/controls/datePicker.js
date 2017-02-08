7b7311a4c57b86c42055a78af3c31bf1a6856842
Nikita Sushkov <nasushkov@gmail.com>
2017-02-08 23:46:07 +0300

    upd

--- src/controls/datePicker.js
+++ src/controls/datePicker.js
@@ -111,7 +111,7 @@ export default class DatePicker extends React.Component {
                 <input type="text" value={inputValue} readOnly={true} className={inputClassName} disabled={disabled} onClick={this.onShow}/>
                 <label htmlFor={props.id} className={labelClassName} data-error={errors}>{props.placeholder}</label>
                 <div style={calerdarContainerStyle} className="collapsible">
-                    {skipRender && <InfiniteCalendar {...calendarProps} selectedDate={props.innerState.value}/>}
+                    {!skipRender && <InfiniteCalendar {...calendarProps} selectedDate={props.innerState.value}/>}
                 </div>
             </div>
         )
